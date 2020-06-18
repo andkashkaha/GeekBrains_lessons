4f8afd6e4ee4456418d595d1d5d44b2229a61326
andkashkaha <66781808+andkashkaha@users.noreply.github.com>
2020-06-18 16:29:59 +0300

    Update Lesson3_Task6.py

--- Lesson3_Task6.py
+++ Lesson3_Task6.py
@@ -1,22 +1,58 @@
 # Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же, но с прописной первой буквой.
-# Например, print(int_func(‘text’)) -> Text.
 # Продолжить работу над заданием. В программу должна попадать строка из слов, разделенных пробелом.
 # Каждое слово состоит из латинских букв в нижнем регистре.
-# Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
-# Необходимо использовать написанную ранее функцию int_func().
 
+def check_latin(k):
+    """Проверка на латинские буквы"""
+    a=1 #если эта переменная меняется, значит проверка не прошла
+    for z in k:
+        if z in set(chr(i) for i in range (ord('a'),ord('z')+1)):
+            continue
+        else:
+            a=0
+            break
+
+    if a==1:
+        return True
+    else:
+        return False
+
+#Сама функция
 def int_func(word):
     """По условию задачи функция может принимать слово только маленькими латинскими буквами"""
-    if word.islower() and word.
+    if word.islower() and check_latin(k=word):
+        first_lit=word[0].upper()
+        another_lit=word[1:]
+        result=first_lit+another_lit
+        return result
+    else:
+        return "#Bad arguments"
 
-
-
-    first_lit=word[0].upper()
-    another_lit=word[1:]
-    result=first_lit+another_lit
-    return result
-
-# Сначала пользователь вводит слово. Все, что после пробела отрезаем, проверяем на латинские буквы
 while True:
-    user_word = input("Введите слово: ").lower()
-    print(int_func(user_word))
+    user_word = input("Введите слово латинскими маленькими буквами: ")
+    res=int_func(user_word)
+    if res=="#Bad arguments":
+        print("Условия не соблюдены. Попробуйте еще раз")
+    else:
+        print(res)
+        break
+
+while True:
+    user_string=input("Введите много слов через пробел латинскими маленькими буквами: ")
+    new_list=[]
+    for z in user_string.split():
+        res=int_func(z)
+        if res=="#Bad arguments":
+            print("Условия не соблюдены. Попробуйте еще раз")
+            break
+        else:
+            new_list.append(res)
+    print(new_list)
+    break
+
+print("Конец")
+
+
+## Тут очень сыро все. Знаю ,что нужно доработать
+
+
